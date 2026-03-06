674a50cdd8cad6d3d89d7a0f571c296207ed29dc
jatinsai <jatinsai09@gmail.com>
2026-03-06 21:04:07 +0530

    implemented login and logout

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import { useAuthStore } from "../store/useAuthStore";
 
 function ChatPage() {
-  return <div>ChatPage</div>;
+  const { logout } = useAuthStore();
+  return (
+    <div className="z-10">
+      ChatPage
+      <button onClick={logout}>logout</button>
+    </div>
+  );
 }
-
 export default ChatPage;
